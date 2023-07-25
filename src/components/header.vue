c9702160150081de0ae5839578d9cd3d12888330
lixiaodong <1078285789@qq.com>
2023-07-25 16:06:12 +0800

    one

---
+++ src/components/header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="header">
+
+    </div>
+</template>
+
+<script>
+import {defineComponent} from 'vue'
+export default defineComponent({
+    
+})
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="home">
+        
+    </div>
+</template>
+
+<script>
+import {defineComponent} from 'vue'
+    export default defineComponent({
+        name: 'home_',
+    })
+</script>
+
+<style lang="less" scoped>
+
+</style>
